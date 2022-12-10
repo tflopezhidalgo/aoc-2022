b65c3629e9f6467697eaea7bc0553e9cb57ccd13
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2022-12-10 20:39:03 -0300

    Move inputs to separate files

--- day-3.cljs
+++ day-3.cljs
@@ -20,10 +20,12 @@
 
       :else (print "Ouch, you should not be here"))))
 
-(defn get-shared-item [[c1 c2]]
-  (let [return (clojure.set/intersection (set c1) (set c2))]
-    (println "Shared item between" c1 "and" c2 "is" return)
-    (first return)))
+(defn get-shared-item [xs]
+  (let [intersection (apply clojure.set/intersection (map set xs))]
+    (println "Shared item in" xs "is" intersection)
+
+    ;; it's a seq
+    (first intersection)))
 
 (defn process-rucksack
   [rucksack]
@@ -31,9 +33,20 @@
         [c1 c2] (partition (/ rucksack-size 2) rucksack)]
     (get-item-priority (get-shared-item [c1 c2]))))
 
+(defn process-group-rucksacks
+  [rucksacks]
+  (let [shared-item (get-shared-item rucksacks)]
+    (get-item-priority shared-item)))
+
+(defn process-2
+  [rucksacks]
+  (let [groups-rucksacks (partition 3 rucksacks)]
+    (apply + (map process-group-rucksacks groups-rucksacks))))
+
 (defn- main
   []
   (let [input (clojure.string/split-lines (slurp "day-3.input"))]
-    (println (apply + (map process-rucksack input)))))
+    (println "1. Answer:" (apply + (map process-rucksack input)))
+    (println "2. Answer:" (process-2 input))))
 
 (main)
